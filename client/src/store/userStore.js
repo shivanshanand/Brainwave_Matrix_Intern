85d5a935cd30110a5efd2c8893f31a84003f67e2
Shivansh Anand <shivanshanand962@gmail.com>
2025-08-10 22:24:41 +0530

    added baseurl in store

--- client/src/store/userStore.js
+++ client/src/store/userStore.js
@@ -2,7 +2,9 @@
 import { create } from "zustand";
 import axios from "axios";
 
-const API = import.meta.env.VITE_API_URL;
+const BASE_URL =
+  import.meta.env.MODE === "development" ? import.meta.env.VITE_API_URL : "/";
+const API = BASE_URL;
 
 export const useUserStore = create((set, get) => ({
   user: null,
