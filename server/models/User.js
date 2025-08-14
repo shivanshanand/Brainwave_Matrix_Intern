e417d9c4b088019478bdb2fd1ac6299d414d6eb7
Shivansh Anand <shivanshanand962@gmail.com>
2025-08-14 12:42:17 +0530

    fixed curr user auth

--- server/models/User.js
+++ server/models/User.js
@@ -16,7 +16,7 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: [true, "Password is required!"],
     },
-    isVerified: { type: Boolean, default: false },
+    isVerified: { type: Boolean, default: true },
   },
   { timestamps: true }
 );
